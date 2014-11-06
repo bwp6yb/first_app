f8226ff552fc28e21bf7219a44303e819e4b8ef6
Brandt Parker <bwp6yb@mail.missouri.edu>
2014-11-06 11:30:59 -0600

    About page design

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 FirstApp::Application.routes.draw do
   root 'static_pages#home'
   match '/pictures',  to: 'static_pages#pictures',  via: 'get'
+  match '/about',     to: 'static_pages#about',     via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
